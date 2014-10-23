b47bec74eb9ae8dc6013c4ce59e23906f7ad280f
Hech <tryctor@gmail.com>
2014-10-22 18:26:55 -0700

    initial commit

--- app/src/main/java/com/hech/musicplayer/MainActivity.java
+++ app/src/main/java/com/hech/musicplayer/MainActivity.java
@@ -5,18 +5,15 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+
+import static com.hech.musicplayer.R.id.action_settings;
 
 
 public class MainActivity extends Activity
@@ -72,7 +69,7 @@ public class MainActivity extends Activity
 
     public void restoreActionBar() {
         ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        assert actionBar != null;
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setTitle(mTitle);
     }
@@ -97,7 +94,7 @@ public class MainActivity extends Activity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
