fa002d992b49801ca5cdf327a386760d6fdfe8fc
mnuthai <mnuthai@gmail.com>
2014-11-05 20:19:40 -0800

    Added a StoreFragment

--- app/src/main/java/com/hech/musicplayer/StoreFragment.java
+++ app/src/main/java/com/hech/musicplayer/StoreFragment.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class StoreFragment extends Fragment {
+    private ListView storeView;
+    private View StoreFragmentView;
+
     public StoreFragment(){}
     @Override
     public View onCreateView (LayoutInflater inflater,
@@ -14,6 +18,25 @@ public class StoreFragment extends Fragment {
                               Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_store,
                         container, false);
+        StoreFragmentView = view;
+        // Get the store view
+        storeView = (ListView)view.findViewById(R.id.store_list);
+
+        setHasOptionsMenu(true);
         return view;
+    }
+
+    // Get all the songs from the database and show on the device
+    public void getOnlineSongList() {
+
+    }
+
+    // Fetch the price of the song (needs to also work for albums)
+    public void getSongPrice() {
+
+    }
+
+    // Deducts the song price from the account
+    public void payment(String user) {
     }
 }
