56741b19ae155b027491a8ed072897f2be894359
Priscilla Vuong <pvuon002@ucr.edu>
2014-11-08 16:53:02 -0800

    Created the Login Screen in LoginFragment.java and xml file. One small error: findViewbyId(int)

--- app/src/main/java/com/hech/musicplayer/LoginFragment.java
+++ app/src/main/java/com/hech/musicplayer/LoginFragment.java
@@ -7,13 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginFragment extends Fragment {
     private EditText  username=null;
     private EditText  password=null;
-    private TextView attempts;
+
 
     public LoginFragment(){};
 
@@ -21,25 +22,42 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+        username= (EditText)view.findViewById(R.id.userText);
+        password= (EditText)view.findViewById(R.id.pwText);
+
+        final Button nbutton =
+                (Button) view.findViewById(R.id.newButton);
+        nbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                newLoginCheck(v);
+            }
+        });
+
+        final Button ebutton =
+                (Button) view.findViewById(R.id.userButton);
+        ebutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                userLoginCheck(v);
+            }
+        });
         return view;
     }
 
     public void userLoginCheck(View view){
-        username=(EditText)view.findViewById(R.id.userText);
-        password= (EditText)view.findViewById(R.id.pwText);
         String usern= username.getText().toString();
         String pw= password.getText().toString();
         Log.d("Username:",usern);
         Log.d("Password:",pw);
+        Toast.makeText(getActivity().getApplicationContext(), usern + " " + pw,
+                Toast.LENGTH_SHORT).show();
 
         //if login fail: SUSIE CHECK DB FOR matching username/pw
+        if (false)
         Toast.makeText(getActivity().getApplicationContext(), "Wrong Credentials",
                 Toast.LENGTH_SHORT).show();
     }
 
     public void newLoginCheck(View view){
-        username=(EditText)view.findViewById(R.id.userText);
-        password= (EditText)view.findViewById(R.id.pwText);
         String usern= username.getText().toString();
         String pw= password.getText().toString();
 
