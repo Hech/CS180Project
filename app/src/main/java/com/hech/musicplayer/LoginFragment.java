8ae2f2119cda6a23648021cfb7408db5c7ef24f0
Hech <tryctor@gmail.com>
2014-11-10 20:27:33 -0800

    added lib file: Parse library

--- app/src/main/java/com/hech/musicplayer/LoginFragment.java
+++ app/src/main/java/com/hech/musicplayer/LoginFragment.java
@@ -14,7 +14,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
